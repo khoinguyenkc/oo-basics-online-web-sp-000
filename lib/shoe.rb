c9d2048dc571e13f023e4d0cbf23330826c320e8
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-17 10:11:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,14 @@
 class Shoe
 attr_reader :brand
 attr_accessor :color, :material, :size, :condition
+
 def initialize(brand)
   @brand = brand
 end
 
-
+def cobble
+  puts "Your shoe is as good as new!"
+end
 
 
 
