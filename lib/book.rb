76d08ba394d53f4b60cf8992dd3de32f74045d5a
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-17 10:08:51 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,40 +3,10 @@
 class Book
 
 #if u want the ability to do book.new(arguments), need to set up the parameters in initialize
-
+attr_accessor :author, :genre, :page_count
+attr_reader :title
 def initialize(title)
   @title = title
-end
-
-
-def title
-  @title
-end
-
-
-def author=(author)
-  @author = author
-end
-
-def author
-  @author
-end
-
-def page_count=(num)
-  @page_count = num
-end
-
-
-def page_count
-  @page_count
-end
-
-def genre=(genre)
-  @genre = genre
-end
-
-def genre
-  @genre
 end
 
 
