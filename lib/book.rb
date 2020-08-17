d83b525e5bab6cbb3105586b4bd1dadaa2b50b56
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-17 10:01:23 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
 
 
+class Book
+
+#if u want the ability to do book.new(arguments), need to set up the parameters in initialize
+
+def initialize(title)
+  @title = title
+end
+
+
+def title
+  @title
+end
+
+
+
+
+def title
+
+
+
+end
